3f73b5d9d3e323736eee0807c20ca4190aadeef1
user <iOS-School-1@iMac-user-4.local>
2017-06-15 21:46:15 +0300

    Initial commit

--- MockTest/ViewController.m
+++ MockTest/ViewController.m
@@ -10,20 +10,42 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UITextField *tf1;
+@property (nonatomic, strong) UITextField *tf2;
+@property (nonatomic, strong) UIButton *button1;
+@property (nonatomic, strong) UITextField *res;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.button1 = [[UIButton alloc] initWithFrame: CGRectMake(20, 20, 40, 50)];
+    self.button1.backgroundColor = [UIColor blueColor];
+    [self.button1 addTarget:self
+                action:@selector(button1Press)
+      forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.button1];
+    
+    self.tf1 = [[UITextField alloc] initWithFrame:CGRectMake(20, 80, 150, 50)];
+    self.tf1.backgroundColor = [UIColor whiteColor];
+    self.tf2 = [[UITextField alloc] initWithFrame:CGRectMake(20, 140, 150, 50)];
+    self.tf2.backgroundColor = [UIColor whiteColor];
+    self.res = [[UITextField alloc] initWithFrame:CGRectMake(20, 200, 150, 50)];
+    self.res.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.tf1];
+    [self.view addSubview:self.tf2];
+    [self.view addSubview:self.res];
+
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)button1Press {
+    NSInteger arg1 = self.tf1.text.integerValue;
+    NSInteger arg2 = self.tf2.text.integerValue;
+    NSInteger result = arg1 * arg2;
+    self.res.text = [NSString stringWithFormat:@"%ld", (long)result];
 }
-
 
 @end
--- MockTestTests/MockTestTests.m
+++ MockTestTests/MockTestTests.m
@@ -7,8 +7,23 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "OCMock.h"
+#import "ViewController.h"
+
+@interface ViewController (TestPrivate)
+
+@property (nonatomic, strong) UITextField *tf1;
+@property (nonatomic, strong) UITextField *tf2;
+@property (nonatomic, strong) UITextField *res;
+
+- (void)button1Press;
+
+@end
+
 
 @interface MockTestTests : XCTestCase
+
+@property(nonatomic,strong) id controllerMock;
 
 @end
 
@@ -16,17 +31,29 @@
 
 - (void)setUp {
     [super setUp];
+    ViewController *controller = [[ViewController alloc] init];
+    self.controllerMock = OCMPartialMock(controller);
+    [self.controllerMock setRes:[UITextField new]];
+
+    id field1Mock = OCMPartialMock([UITextField new]);
+    id field2Mock = OCMPartialMock([UITextField new]);
+
+    OCMStub([field1Mock text]).andReturn(@"3");
+    OCMStub([field2Mock text]).andReturn(@"21");
+    [self.controllerMock setTf1:field1Mock];
+    [self.controllerMock setTf2:field2Mock];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    self.controllerMock = nil;
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    [self.controllerMock button1Press];
+    XCTAssertTrue([@"63" isEqualToString:[[self.controllerMock res] text]], @"Test button press");
 }
 
 - (void)testPerformanceExample {
